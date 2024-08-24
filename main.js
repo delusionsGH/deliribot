142af9a1869c97979bcca260e89066fcab88181f
delusions <mogusblaster@gmail.com>
2024-08-24 12:54:19 +0100

    add deliribot

---
+++ main.js
@@ -0,0 +1,82 @@
+import { RoarBot } from "@mbw/roarbot";
+import chalk from "npm:chalk";
+import { initChat } from "@mumulhl/duckduckgo-ai-chat/";
+const log = console.log;
+log(chalk.blue(`setting ai model...`));
+const ai = await initChat("gpt-4o-mini");
+const aiModel = "gpt-4o-mini";
+log(chalk.blue(`set model to ${aiModel}.`));
+const bot = new RoarBot({
+    admins: ["delusions"],
+});
+
+bot.command("about", { // sample command, copy for new commands
+    args: [],
+    fn: async function (reply, _args, _post) {
+        await reply(
+            '```\ndeliribot (previously zzbot.js)\nversion v2.1.0 (230824)\nroarbot made by mybearworld, deliribot by delusions\n\nlatest command added: ai\n"does the ai thing"',
+        );
+    },
+});
+bot.command("whois", { // looks for the data of a user, YES I KNOW "fetc" IS WEIRD but mbw told me not to use fetch :(
+    args: [{ name: "name", type: "string" }],
+    fn: async function (reply, [name], _post) {
+        let fetc = await bot.user(name); // fetch the userdata from api/users/(user)
+        log(chalk.blue(`fetching @${name}'s userdata...`));
+        try {
+            fetc = await bot.user(name);
+        } catch (_error) {
+            return;
+        }
+        log(chalk.green(`fetched @${name}'s userdata`));
+        const user = name;
+        const id = fetc.uuid;
+        const avatarcolor = `#${fetc.avatar_color}`;
+        const quote = `\"${fetc.quote}\"`;
+        const avatar = fetc.avatar
+        log(chalk.blue(`combining data...`));
+        const result = `# ***@${user}***\n[link to profile picture](https://uploads.meower.org/icons/${avatar})\n\n**Quote:**\n${quote.replace(/^/gm, "> ")}\n\n**UUID:** "${id}"\n**Avatar's hex code:**  ${avatarcolor}`; // combine it all
+        await reply(result);
+        log(chalk.green.bold(`whois successfully run!`)); // yay it worked
+    },
+});
+bot.command("error", {
+    args: [],
+    admin: true,
+    fn: async function (reply, _args, post) {
+        if (post.username === admin) {
+            for (let i = 0; i < 3; i++) {
+                await reply("Uncaught error!");
+            }
+        }
+    },
+});
+bot.command("exit", {
+    args: [],
+    admin: true,
+    fn: async function (reply, _args, _post) {
+            await reply("Shutting down zzbot.js.");
+            Deno.exit();
+        }
+    },
+);
+bot.command("echo", { // does the bot work? if this command works, it certainly does, at least partially
+    args: [{ name: "message", type: "full" }],
+    admin: true,
+    fn: async function (reply, args, _post) {
+        await reply(args[0]);
+    },
+});
+bot.command("ai", { // ai chat
+    args: [{ name: "message", type: "full" }],
+    admin: true,
+    fn: async function (reply, [message], _post) {
+            log(chalk.blue(`sending message ("${message}")...`));
+            const aimessage = await ai.fetchFull(message);
+            await reply(aimessage);
+            log(chalk.green.bold(`${aiModel} replied with: ${aimessage}`));
+        }
+    },
+);
+const config = JSON.parse(await Deno.readTextFile("config.json"));
+bot.login("deliribot", config.botPassword);
---
+++ readme.md
@@ -0,0 +1,14 @@
+yes this code is terrible, and admittedly, i dont care
+yes this code is a mess
+yes i copied the idea of this comment
+yes i wont do anything about it
+-zz
+
+how to start up this mess:
+1. cd to the file's directory in cmd
+2. do `deno run --allow-all main.js`
+3. if it nags you to update, shut the bot down with ctrl+c and do `deno add @mbw/roarbot@latest`
+4. otherwise, it's running
+
+zzbot.js created by delusions
+roarbot created by mbw (@mybearworld on meower)
